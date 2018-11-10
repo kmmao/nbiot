636b8a8bf712901f8991f8bffc81a626e5579ef1
mzkun <mzkuncool@126.com>
2018-11-10 20:35:44 +0800

    springboot 多模块项目提交

---
+++ nb-server/src/main/java/com/goldcard/nbiot/NbServerApplication.java
@@ -0,0 +1,29 @@
+package com.goldcard.nbiot;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ----------------------------------------------------------------
+ * Copyright (C) 2017 金卡高科技股份有限公司
+ * 版权所有。
+ * <p>
+ * 文件名：NbServerApplication.java
+ * 文件功能描述：
+ * <p>
+ * 创建标识 mzkun
+ * 创建时间 2018/10/31 09:23
+ * <p>
+ * 修改标识：
+ * 修改描述：
+ * ----------------------------------------------------------------
+ */
+//@SpringBootApplication
+//@EnableAdminServer
+//@Slf4j
+public class NbServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NbServerApplication.class, args);
+    }
+}
